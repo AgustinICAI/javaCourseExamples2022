076a3f8814a7f26343ebaa3ad1f02d919904a308
agus <a.g.lamela@gmail.com>
2022-04-07 20:42:37 +0200

    subiendo código corregido threads día 6-4

--- 12c.Threads/AppVentana.java
+++ 12c.Threads/AppVentana.java
@@ -1,11 +1,8 @@
 import javax.swing.*;
+import javax.swing.Timer;
 import java.awt.*;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.TreeSet;
+import java.awt.event.*;
+import java.util.*;
 
 public class AppVentana extends JFrame {
     private JTextArea txt;
@@ -23,6 +20,9 @@ public class AppVentana extends JFrame {
     public AppVentana(){
         super("Ventana gestionando hilos");
 
+
+
+
         txt = new JTextArea();
         JScrollPane jsp = new JScrollPane(txt);
         jsp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -31,13 +31,85 @@ public class AppVentana extends JFrame {
         txt.setFont(new Font("Sans Serif",Font.PLAIN,30));
         this.setSize(800,800);
         this.setVisible(true);
-        for (int i = 0; i < 10000; i++) {
-            Thread hilo = new MiHilo(this);
-            hilo.start();
+        ArrayList<Thread> hilos = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            hilos.add(new MiHilo(this,i));
+        }
+        hilos.forEach(h -> h.start());
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(AppVentana.this.isDisplayable()){
+                    try {
+                        Random r = new Random();
+                        txt.setBackground(new Color(r.nextInt(255),
+                                r.nextInt(255),r.nextInt(255)));
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        t1.start();
+        /*
+        new Thread( () -> {
+            while(AppVentana.this.isDisplayable()){
+                    try {
+                        Random r = new Random();
+                        txt.setBackground(new Color(r.nextInt(255),
+                                r.nextInt(255),r.nextInt(255)));
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        }).start();
+         */
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+
+            }
+        });
+
+        new Timer(300, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.out.println("HOla");
+            }
+        });
+        new Timer(300, actionEvent -> System.out.println("Adios") );
+
+        JButton btn = new JButton();
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+
+        btn.addActionListener(actionEvent -> System.out.println("He pulsado el botón"));
+
+        HashMap<String,String> hm = new HashMap<>();
+
+        for (Map.Entry<String,String> m : hm.entrySet() ){
+            System.out.println(m.getKey() + "-" + m.getValue());
         }
 
-        Thread hiloColorin = new MiHiloColorin(this);
-        hiloColorin.start();
+
+        hm.forEach( (k,v) -> System.out.println(k + "-" + v) );
+
+
+        //Thread hiloColorin = new MiHiloColorin(this);
+        //hiloColorin.start();
 
 
         this.addWindowListener(new WindowAdapter() {
@@ -47,11 +119,17 @@ public class AppVentana extends JFrame {
             }
         });
 
-        //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 
     public JTextArea getTxt() {
         return txt;
     }
+
+    synchronized public void append(String cad){
+        this.txt.append(cad);
+    }
+
+
 }
--- 12c.Threads/MiHilo.java
+++ 12c.Threads/MiHilo.java
@@ -3,17 +3,27 @@ import java.util.Calendar;
 
 public class MiHilo extends Thread{
 
+    private int idHilo;
     private AppVentana ventana;
 
-    public MiHilo(AppVentana ventana) {
+    public MiHilo(AppVentana ventana, int idHilo) {
         this.ventana = ventana;
+        this.idHilo = idHilo;
     }
 
     @Override
     public void run(){
-        while (ventana.isDisplayable()) {
-            ventana.getFechas().add(Calendar.getInstance().getTime().toString() + "\n");
-            System.out.println(Calendar.getInstance().getTime().toString());
+        //int inicio = (int) (System.nanoTime()/1000000000);
+        for(int i = 0 ; i<100; i++) {
+            //long tiempoEntreMedias = System.nanoTime() / 1000000000 - inicio;
+
+
+            //CALENDAR.GETINSTANCE es Thread Safe
+            //https://www.daniweb.com/programming/software-development/threads/280289/is-calender-getinstance-method-is-thread-safe#post1206617
+
+            //ventana.getFechas().add(Calendar.getInstance().getTime().toString() + "\n");
+            ventana.append("Hilo " + idHilo + " mostrando " + i + "\n");
+            //System.out.println(Calendar.getInstance().getTime().toString());
 
             /*try {
                 Thread.sleep(5);
@@ -21,6 +31,7 @@ public class MiHilo extends Thread{
                 e.printStackTrace();
             }*/
         }
+
     }
 
 }
